18d0e14aa4db0913cef9c276bb1487367f61c4e4
young1015 <youngrobertbzs@gmail.com>
2023-10-04 09:47:12 -0300

    Criação do Servlet

---
+++ src/main/java/main/java/mediconnect/controle/Servlet.java
@@ -0,0 +1,28 @@
+package main.java.mediconnect.controle;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/")
+public class Servlet extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+
+	}
+
+}
