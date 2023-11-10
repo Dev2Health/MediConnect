346ceda08300eb45fdbd973fad9ee139b743908c
Antonio Luis <antonioluismoreiralima01@gmail.com>
2023-11-10 11:24:04 -0300

    Criação do script 'usuario-login'

--- src/main/webapp/assets/scripts/usuario-login.js
+++ src/main/webapp/assets/scripts/usuario-login.js
@@ -3,11 +3,25 @@ function mostrarSenha() {
    const botao = document.getElementById("botao-senha");
 
    if (senha.type === "password") {
-    senha.setAttribute("type", "text");
-    botao.src = "../imagens/icone-olho-aberto.svg";
+      senha.setAttribute("type", "text");
+      botao.src = "../imagens/icone-olho-aberto.svg";
    }
    else {
-    senha.setAttribute("type", "password");
-    botao.src = "../imagens/icone-olho-fechado.svg";
+      senha.setAttribute("type", "password");
+      botao.src = "../imagens/icone-olho-fechado.svg";
    }
+}
+
+function mascaraEmail() {
+
+   var formulario = document.getElementById("formulario");
+   var campo = document.getElementById(email);
+
+   formulario.addEventListener("submit", pararEvento);
+   function pararEvento(evento) {
+      if (campo.innerHTML == "")
+         event.preventDefault(); 
+   }
+
+
 }
